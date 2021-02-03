8bb8e2fbbb1d0ade10a168932af3992b48dabf9a
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-03 14:18:53 +0000

    Create two separate examples for rank and suit.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,13 @@
 require 'card.rb'
 
 RSpec.describe Card do
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+  it 'has a rank' do
+    card = Card.new('Ace' 'Spades')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
