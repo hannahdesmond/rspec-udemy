74606d56a3df4ef36b016c184c303a5dfd940eae
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-09 18:19:00 +0000

    Use a before hook to reduce duplication.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,13 +1,15 @@
 require 'card.rb'
 
+
 RSpec.describe Card do
+  before(:example) do
+    @card = Card.new('Ace', 'Spades')
+  end
   it 'has a rank' do
-    card = Card.new('Ace' 'Spades')
-    expect(card.rank).to eq('Ace')
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.suit).to eq('Spades')
+    expect(@card.suit).to eq('Spades')
   end
 end
